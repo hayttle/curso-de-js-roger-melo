8b2e86a2541c176954aef3681e0732dc9758e7ae
hayttle <hayttle@gmail.com>
2022-12-12 21:36:32 -0300

    updated

--- exercicio-39/app.js
+++ exercicio-39/app.js
@@ -41,10 +41,12 @@ const accordion = document.querySelector("[data-js='accordion']")
 accordion.addEventListener("click", (e) => {
   const accordionHeaderId = e.target.dataset.accordionHeader
   const clickedAccordionHeader = document.querySelector(`[data-accordion-header="${accordionHeaderId}"]`)
-  const accordionBody = document.querySelector(`[data-accordion-body="${accordionHeaderId}"]`)
+  const accordionItemToBeOpened = document.querySelector(`[data-accordion-body="${accordionHeaderId}"]`)
 
-  clickedAccordionHeader.classList.toggle("active")
-  accordionBody.classList.toggle("active")
+  if (accordionHeaderId) {
+    clickedAccordionHeader.classList.toggle("active")
+    accordionItemToBeOpened.classList.toggle("active")
+  }
 })
 
 /*
